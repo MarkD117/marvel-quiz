84fb70c59614ad2dc72d3a4ab6c844d7977d850a
Mark Dunne <Markdunne117@gmail.com>
2023-05-21 18:16:24 +0000

    Add code for entering quiz

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,16 @@
-function enterQuizBtn() {
+const enterQuizBtn = document.getElementById('enter-quiz-btn');
+const quizHomeMenu = document.getElementById('quiz-home-menu');
+const difficultySelectorMenu = document.getElementById('quiz-difficulty-selection');
 
-}
+
+/**
+ * Event listener added to the quiz button on the quiz home screen
+ * that listens for clicks. It will bring the user to the difficulty selection screen 
+ */
+enterQuizBtn.addEventListener('click', function () {
+    quizHomeMenu.classList.add("hidden");
+    difficultySelectorMenu.classList.remove("hidden");
+})
 
 function difficultySelector() {
 
