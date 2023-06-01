111c63640e3c375402da87aabdc7cdd47d0aa349
Mark Dunne <Markdunne117@gmail.com>
2023-06-01 00:12:33 +0000

    Moved quiz questions to questions.js file and linked to index.html

---
+++ assets/js/questions.js
@@ -0,0 +1,188 @@
+// Easy Questions nested data structure
+const easyQuestions = [
+    {
+        question: 'What was the first Marvel movie ever made?',
+        options: [
+            { text: 'Iron Man',},
+            { text: 'Thor',},
+            { text: 'Captain America',},
+            { text: 'The Incredible Hulk',}
+        ],
+
+        answer: 'Iron Man'
+    },
+
+    {
+        question: "What is Black Panther's suit made of?",
+        options: [
+            { text: 'Titanium'},
+            { text: 'Carbon Fibre'},
+            { text: 'Vibranium'},
+            { text: 'Kevlar'}
+        ],
+
+        answer: 'Vibranium'
+    },
+
+    {
+        question: 'Which actor voices the character of Groot?',
+        options: [
+            { text: 'Bradley Cooper'},
+            { text: 'Vin Diesel'},
+            { text: 'Jeremy Renner'},
+            { text: 'Mark Ruffalo'}
+        ],
+
+        answer: 'Vin Diesel'
+    },
+
+    {
+        question: 'What is Korg made of?',
+        options: [
+            { text: 'Iron'},
+            { text: 'Scissors'},
+            { text: 'Paper'},
+            { text: 'Rock'}
+        ],
+
+        answer: 'Rock'
+    },
+
+    {
+        question: "What is Tony Stark's father's name?",
+        options: [
+            { text: 'Howard'},
+            { text: 'Steve'},
+            { text: 'James'},
+            { text: 'Peter'}
+        ],
+
+        answer: 'Howard'
+    },
+]
+
+// Medium Questions nested data structure
+const mediumQuestions = [
+    {
+        question: 'Which billionaire makes a cameo in Iron Man 2?',
+        options: [
+            { text: 'Richard Branson'},
+            { text: 'Elon Musk'},
+            { text: 'Mark Zuckerberg'},
+            { text: 'Jeff Bezos'}
+        ],
+
+        answer: 'Elon Musk'
+    },
+
+    {
+        question: "Who is Black Panther's little sister",
+        options: [
+            { text: 'Shuri'},
+            { text: 'Okoye'},
+            { text: 'Nakia'},
+            { text: 'Ramonda'}
+        ],
+
+        answer: 'Shuri'
+    },
+
+    {
+        question: 'What object does Goose swallow in Captain Marvel?',
+        options: [
+            { text: 'A transponder'},
+            { text: "Fury's eye patch"},
+            { text: 'The Tesseract'},
+            { text: 'Ship keys'}
+        ],
+
+        answer: 'The Tesseract'
+    },
+
+    {
+        question: 'What were the last words said by Thanos in Avengers: Endgame?',
+        options: [
+            { text: 'I am a survivor'},
+            { text: 'A small price to pay for salvation'},
+            { text: 'Perfectly balanced as all things should be'},
+            { text: 'I am inevitable'}
+        ],
+
+        answer: 'I am inevitable'
+    },
+
+    {
+        question: 'Which Avenger does Loki enslave in The Avengers?',
+        options: [
+            { text: 'Hawkeye'},
+            { text: 'Hulk'},
+            { text: 'Iron Man'},
+            { text: 'Captain America'}
+        ],
+
+        answer: 'Hawkeye'
+    },
+]
+
+// Hard Questions nested data structure
+const hardQuestions = [
+    {
+        question: 'What type of doctor is Dr. Strange?',
+        options: [
+            { text: 'Neurosurgeon'},
+            { text: 'Orthopaedic surgeon'},
+            { text: 'General surgeon'},
+            { text: 'Cardiothoracic surgeon'}
+        ],
+
+        answer: 'Neurosurgeon'
+    },
+
+    {
+        question: "What animal does Darren Cross unsuccessfully use as test subjects in Ant-Man?",
+        options: [
+            { text: 'Rats'},
+            { text: 'Lambs'},
+            { text: 'Rabbits'},
+            { text: 'Goats'}
+        ],
+
+        answer: 'Lambs'
+    },
+
+    {
+        question: 'Which Spongebob character does Tony call Ebony Maw in Avengers: Infinity War?',
+        options: [
+            { text: 'Patrick'},
+            { text: "Krabs"},
+            { text: 'Plankton'},
+            { text: 'Squidward'}
+        ],
+
+        answer: 'Squidward'
+    },
+
+    {
+        question: 'Who was the main foe in Thor: Dark World?',
+        options: [
+            { text: 'Malekith'},
+            { text: 'Kurse'},
+            { text: 'Collector' },
+            { text: 'Volstagg'}
+        ],
+
+        answer: 'Malekith'
+    },
+
+    {
+        question: "How many legs does Odin's horse have?",
+        options: [
+            { text: 'Four'},
+            { text: 'Ten'},
+            { text: 'Eight'},
+            { text: 'Six'}
+        ],
+
+        answer: 'Eight'
+    },
+]
